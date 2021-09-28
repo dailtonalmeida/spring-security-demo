bc8353f6d619b57344b9209658de8d87f03c0d71
Dailton Santana de Almeida <dailton.almeida@gft.com>
2021-09-28 17:37:13 -0300

    #bInMemoryAuthenticationProvider - autenticando contra inMemoryAuthentication -- usuarios e senhas no codigo java

--- src/main/java/br/nom/dsa/springbootsoujava/config/InMemorySecurityConfig.java
+++ src/main/java/br/nom/dsa/springbootsoujava/config/InMemorySecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
  *
  * @author dailtonalmeida
  */
-//@EnableWebSecurity
+@EnableWebSecurity
 public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
