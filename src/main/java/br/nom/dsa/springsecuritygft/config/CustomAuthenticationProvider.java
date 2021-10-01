4c164087cb79db85ec4c8c54d87c119e6d56ded8
Dailton Santana de Almeida <dailton.almeida@gft.com>
2021-10-01 17:06:19 -0300

    #bCustomAuthenticationProvider renomeando pacotes

--- src/main/java/br/nom/dsa/springsecuritygft/config/CustomAuthenticationProvider.java
+++ src/main/java/br/nom/dsa/springsecuritygft/config/CustomAuthenticationProvider.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.nom.dsa.springbootsoujava.config;
+package br.nom.dsa.springsecuritygft.config;
 
 import java.io.File;
 import java.io.FileInputStream;
--- src/main/java/br/nom/dsa/springsecuritygft/config/CustomSecurityConfig.java
+++ src/main/java/br/nom/dsa/springsecuritygft/config/CustomSecurityConfig.java
@@ -1,9 +1,11 @@
+package br.nom.dsa.springsecuritygft.config;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.nom.dsa.springbootsoujava.config;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
